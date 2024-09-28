354256b769314f0e55630632d850f7d98d107845
NateTheUnavailable <nate@stevenogardner.com>
2024-09-27 20:45:30 -0600

    made file smaller

--- Java Game/Reader.java
+++ Java Game/Reader.java
@@ -4,46 +4,39 @@ import java.util.Scanner;
 
 public class Reader {
     public static void start() throws FileNotFoundException{
-        String number = "";
+        String number = "A";
         String D = System.getProperty("user.dir");
         File file = new File(D + "//Game Story//story.txt");
-        File file2 = new File(D + "//Game Story//story2.txt");
-        File file3 = new File(D + "//Game Story//story3.txt");
-        File file4 = new File(D + "//Game Story//story4.txt");
-        File file5 = new File(D + "//Game Story//story5.txt");
-        File file6 = new File(D + "//Game Story//story6.txt");
-        File file7 = new File(D + "//Game Story//story7.txt");
-        File file8 = new File(D + "//Game Story//story8.txt");
-        Scanner scan = new Scanner(file);
         Game reader = new Game();
         reader.story(number);
         if("A".equals(number)){
-            scan = new Scanner(file);
+            file = new File(D + "//Game Story//story.txt");
         }
         if("B".equals(number)){
-            scan = new Scanner(file);
+            file = new File(D + "//Game Story//story2.txt");
         }
         if("C".equals(number)){
-            scan = new Scanner(file);
+            file = new File(D + "//Game Story//story3.txt");
         }
         if("D".equals(number)){
-            scan = new Scanner(file);
+            file = new File(D + "//Game Story//story4.txt");
         }
         if("E".equals(number)){
-            scan = new Scanner(file);
+            file = new File(D + "//Game Story//story5.txt");
         }
         if("F".equals(number)){
-            scan = new Scanner(file);
+            file = new File(D + "//Game Story//story6.txt");
         }
         if("G".equals(number)){
-            scan = new Scanner(file);
+            file = new File(D + "//Game Story//story7.txt");
         }
         if("H".equals(number)){
-            scan = new Scanner(file);
+            file = new File(D + "//Game Story//story8.txt");
         }
         else if("".equals(number)){
-            scan = new Scanner(file);
+            file = new File(D + "//Game Story//story.txt");
         }
+        Scanner scan = new Scanner(file);
         while (scan.hasNextLine())
             System.out.println(scan.nextLine());
     }
