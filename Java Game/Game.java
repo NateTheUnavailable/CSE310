0624988e7eb11783c1af25df1aad2f3193be6a5d
NateTheUnavailable <nate@stevenogardner.com>
2024-09-27 20:13:09 -0600

    working story section sorta

--- Java Game/Game.java
+++ Java Game/Game.java
@@ -1,16 +1,13 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+
+import java.util.Scanner;
 
 public class Game{
     public static void main(String[] args) throws Exception{
         System.out.println("The Story of Dan");
-        AssetManager assetManager = getAssets();
-        InputStream = assetManager.open("text.txt");
-        // Declaring loop variable
-        int i;
-        // Holds true till there is nothing to read
-        while ((i = fr.read()) != -1)
-        System.out.print((char)i);
+        Reader.start();
+    }
+    public void story(String choice){
+        Scanner sc = new Scanner(System.in);
+        choice = sc.nextLine();
     }
   }
--- Java Game/Reader.java
+++ Java Game/Reader.java
@@ -3,10 +3,48 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Reader {
-    public static void main(string[] args) throws FileNotFoundException{
-        File file = new File("Game Story\\story.txt");
+    public static void start() throws FileNotFoundException{
+        String number = "";
+        String D = System.getProperty("user.dir");
+        File file = new File(D + "//Game Story//story.txt");
+        File file2 = new File(D + "//Game Story//story2.txt");
+        File file3 = new File(D + "//Game Story//story3.txt");
+        File file4 = new File(D + "//Game Story//story4.txt");
+        File file5 = new File(D + "//Game Story//story5.txt");
+        File file6 = new File(D + "//Game Story//story6.txt");
+        File file7 = new File(D + "//Game Story//story7.txt");
+        File file8 = new File(D + "//Game Story//story8.txt");
         Scanner scan = new Scanner(file);
-
-        System.out.println(scan.nextLine());
+        Game reader = new Game();
+        reader.story(number);
+        if("A".equals(number)){
+            scan = new Scanner(file);
+        }
+        if("B".equals(number)){
+            scan = new Scanner(file);
+        }
+        if("C".equals(number)){
+            scan = new Scanner(file);
+        }
+        if("D".equals(number)){
+            scan = new Scanner(file);
+        }
+        if("E".equals(number)){
+            scan = new Scanner(file);
+        }
+        if("F".equals(number)){
+            scan = new Scanner(file);
+        }
+        if("G".equals(number)){
+            scan = new Scanner(file);
+        }
+        if("H".equals(number)){
+            scan = new Scanner(file);
+        }
+        else if("".equals(number)){
+            scan = new Scanner(file);
+        }
+        while (scan.hasNextLine())
+            System.out.println(scan.nextLine());
     }
 }
