d8e4dc4bf27a55e8dc7b9b3bc2e072f16d8a0511
NateTheUnavailable <nate@stevenogardner.com>
2024-09-28 18:57:22 -0600

    changes to the choices

--- Java Game/Reader.java
+++ Java Game/Reader.java
@@ -11,27 +11,52 @@ public class Reader {
         reader.story(number);
         if("A".equals(number)){
             file = new File(D + "//Game Story//story.txt");
+            if("A".equals(number)){
+                file = new File(D + "//Game Story//story.txt");
+                if("A".equals(number)){
+                    file = new File(D + "//Game Story//story.txt");
+                    
+                }
+                if("B".equals(number)){
+                    file = new File(D + "//Game Story//story2.txt");
+                    if("A".equals(number)){
+                        file = new File(D + "//Game Story//story.txt");
+                        
+                    }
+                    if("B".equals(number)){
+                        file = new File(D + "//Game Story//story2.txt");
+                    }
+                }
+                
+            }
+            if("B".equals(number)){
+                file = new File(D + "//Game Story//story2.txt");
+                if("A".equals(number)){
+                    file = new File(D + "//Game Story//story.txt");
+                    if("A".equals(number)){
+                        file = new File(D + "//Game Story//story.txt");
+                        
+                    }
+                    if("B".equals(number)){
+                        file = new File(D + "//Game Story//story2.txt");
+                        if("A".equals(number)){
+                            file = new File(D + "//Game Story//story.txt");
+                            
+                        }
+                        if("B".equals(number)){
+                            file = new File(D + "//Game Story//story2.txt");
+                        }
+                    }
+                    
+                }
+                if("B".equals(number)){
+                    file = new File(D + "//Game Story//story2.txt");
+                }
+            }
+            
         }
         if("B".equals(number)){
             file = new File(D + "//Game Story//story2.txt");
-        }
-        if("C".equals(number)){
-            file = new File(D + "//Game Story//story3.txt");
-        }
-        if("D".equals(number)){
-            file = new File(D + "//Game Story//story4.txt");
-        }
-        if("E".equals(number)){
-            file = new File(D + "//Game Story//story5.txt");
-        }
-        if("F".equals(number)){
-            file = new File(D + "//Game Story//story6.txt");
-        }
-        if("G".equals(number)){
-            file = new File(D + "//Game Story//story7.txt");
-        }
-        if("H".equals(number)){
-            file = new File(D + "//Game Story//story8.txt");
         }
         else if("".equals(number)){
             file = new File(D + "//Game Story//story.txt");
